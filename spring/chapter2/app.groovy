d9e05fb657c762841933b190b336481d4a5f5884
佐々木雅哉 <masaya@sasakimasayanoMacBook-Pro.local>
2020-05-15 17:33:11 +0900

    2章

---
+++ spring/chapter2/app.groovy
@@ -0,0 +1,23 @@
+@Grab("thymeleaf-spring5")
+
+@Controller
+class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    def home(ModelAndView mav) {
+        mav.setViewName("home")
+        mav.addObject("msg", "please write your name....")
+        mav
+    }
+
+
+    @RequestMapping(value="/send", method=RequestMethod.POST)
+    @ResponseBody
+    def send(@RequestParam("text1")String str, ModelAndView mav) {
+        mav.setViewName("home")
+        mav.addObject("msg","Hello, " + str + "!!")
+        mav.addObject("value",str)
+        mav
+    }
+}
